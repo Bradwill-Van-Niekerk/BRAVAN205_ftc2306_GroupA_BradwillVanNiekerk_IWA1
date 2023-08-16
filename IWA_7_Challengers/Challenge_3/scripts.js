267562e49a55f81df0d9fb9c92a044602bcca2ba
Bradwill Van Niekerk <bradwill.oostendorp7@gmail.com>
2023-08-16 11:30:50 +0200

    update to 7.3

--- IWA_7_Challengers/Challenge_3/scripts.js
+++ IWA_7_Challengers/Challenge_3/scripts.js
@@ -10,16 +10,11 @@ const divider = '----------------------------------'
 
 // Only change below this line
 const sarahOwed = parseFloat(sarahBalance * -1).toFixed(2)//Math.abs(sarahBalance)
-
 const leoOwed = parseFloat(leoBalance * -1).toFixed(2)
-function splitintoArray (num){
-    return String(num).split('').map(Number)
-}
-const owed = parseFloat(sarahOwed)+ parseFloat(leoOwed)
-const tolalOwed = splitintoArray(owed)
-const leo = `${leoName} ${leoSurname .trim()} (Owed R${leoBalance})`
-const sarah = `${sarahName .trim()} ${sarahSurname} (Owed R${sarahBalance})`
+const owed = "R"+(parseFloat(sarahOwed)+ parseFloat(leoOwed)).toFixed(2);
+const leo = `${leoName} ${leoSurname.trim()} (Owed R${leoOwed})`
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed R${sarahOwed})`
 const total = `Total amount owed: `
-const result = leo +"\n"+ sarah + "\n"+"\n" + divider +"\n"+ total + "R" +tolalOwed[0] +tolalOwed[1]+" "+tolalOwed[2] +tolalOwed[3] + tolalOwed[4] +"\n"+ divider
+const result = leo +"\n"+ sarah + "\n"+"\n" + divider +"\n"+ total + owed.slice(0, 3) +" " +owed.slice(3)+"\n"+ divider
 
 console.log(result)
