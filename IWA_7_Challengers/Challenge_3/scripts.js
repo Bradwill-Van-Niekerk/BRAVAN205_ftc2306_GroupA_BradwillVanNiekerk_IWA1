3e9603605ecac7e0f95b3c7459cd05284e12077a
Bradwill Van Niekerk <bradwill.oostendorp7@gmail.com>
2023-08-16 10:51:19 +0200

    challenge7&8 Upload

--- IWA_7_Challengers/Challenge_3/scripts.js
+++ IWA_7_Challengers/Challenge_3/scripts.js
@@ -9,12 +9,17 @@ const sarahBalance = '-4582.2'
 const divider = '----------------------------------'
 
 // Only change below this line
-const sarahOwed = parseFloat(sarahBalance)
-const leoOwed = parseFloat(leoBalance) 
-const owed = "R" + (sarahOwed + leoOwed)*-1
+const sarahOwed = parseFloat(sarahBalance * -1).toFixed(2)//Math.abs(sarahBalance)
+
+const leoOwed = parseFloat(leoBalance * -1).toFixed(2)
+function splitintoArray (num){
+    return String(num).split('').map(Number)
+}
+const owed = parseFloat(sarahOwed)+ parseFloat(leoOwed)
+const tolalOwed = splitintoArray(owed)
 const leo = `${leoName} ${leoSurname .trim()} (Owed R${leoBalance})`
 const sarah = `${sarahName .trim()} ${sarahSurname} (Owed R${sarahBalance})`
 const total = `Total amount owed: `
-const result = leo +"\n"+ sarah + "\n"+"\n" + divider +"\n"+ total + owed +"\n"+ divider
+const result = leo +"\n"+ sarah + "\n"+"\n" + divider +"\n"+ total + "R" +tolalOwed[0] +tolalOwed[1]+" "+tolalOwed[2] +tolalOwed[3] + tolalOwed[4] +"\n"+ divider
 
 console.log(result)
